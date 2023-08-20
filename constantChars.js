3272f9815ce68421cbc7d1c6d0df5c4b88e624f6
ArjunJayakrishnan <132384081+ArjunJayakrishnan@users.noreply.github.com>
2023-08-20 19:01:40 +0530

    Add files via upload

---
+++ constantChars.js
@@ -0,0 +1,6 @@
+export const MAX_CHARS_LAPTOP = 80;
+export const MAX_CHARS_MOBILE = 40;
+export const MAX_CHARS_SMALL_MOBILE = 30;
+export const WINDOW_WIDTH_LAPTOP = 575;
+export const WINDOW_WIDTH_MOBILE = 410;
+export const WINDOW_WIDTH_SMALL_MOBILE = 335;
